83b0600ce8ce83d04afc4512e8474759ccd0c4bb
Chris Marshall <cmarshall743@gmail.com>
2018-03-16 23:56:30 -0700

    Movement code 0.1.2
    Added swerve bot using xbox joystick input (white cube)

---
+++ Assets/Scripts/controllerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class controllerMovement : MonoBehaviour {
+
+	public float moveSpeed;
+	public float turnSpeed;
+
+	void Start ()
+	{
+		moveSpeed = 10f;
+		turnSpeed = 200f;
+	}
+
+	void Update ()
+	{
+		transform.Translate (moveSpeed*Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, moveSpeed*Input.GetAxis ("Vertical") * Time.deltaTime, Space.World);
+
+		Rotate (Input.GetAxis ("Rotate"));
+
+	}
+	void Rotate(float input)
+	{
+		if (Math.Abs (input) > 0.25)
+			transform.Rotate (0f, turnSpeed * input * Time.deltaTime, 0f);
+		else
+			transform.Rotate (0f, 0f, 0f);
+	}
+}
