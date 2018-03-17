4d538312320c84b2b211baf5c738dceb78f198f7
Chris Marshall <cmarshall743@gmail.com>
2018-03-16 21:48:31 -0700

    Basic movement v0.1
    First version with basic player movement

---
+++ power up sim/Assets/Scripts/RobotMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotMovement : MonoBehaviour {
+
+	public float moveSpeed;
+	public float Acceleration;
+
+	void Start ()
+	{
+		moveSpeed = 10f;
+	}
+
+	void Update ()
+	{
+		transform.Translate (moveSpeed*Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, moveSpeed*Input.GetAxis ("Vertical") * Time.deltaTime);
+	}
+}
